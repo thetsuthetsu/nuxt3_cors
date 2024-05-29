f4ea56cca28f2767bd7eb3518090e175e239f279
hino <tetsuo.hino@veriserve.co.jp>
2024-05-29 18:18:02 +0900

    first commit

---
+++ composables/useCpeMatching.js
@@ -0,0 +1,32 @@
+export async function useCpeMatching (url, key) {
+    // $fetchはserver(HTMLレンダリング）とclient(HTMLハイドレード)のそれぞれで計二回実行される可能性がある
+    // useAsyncData、またはuseFetchを使う。(https://nuxt.com/docs/getting-started/data-fetching)
+    // ただし、useFetchをイベントハンドラで呼び出すと以下の警告が発生。
+    // useCpeMatching.js:5 [nuxt] [useFetch] Component is already mounted, please use $fetch instead. See https://nuxt.com/docs/getting-started/data-fetching
+
+    // const { data: products, error } = await useFetch(url)
+    // if (products.value) {
+    //     console.log(`products[${JSON.stringify(products.value)}]`)
+    // }
+    // if (error.value) {
+    //     console.log(`error[${JSON.stringify(error.value)}]`)
+    // }
+    try {
+
+        if (key) {
+            // headerを指定するとCORSエラーが発生。
+            const products = await $fetch(url, {
+                headers: {
+                    common: {'apiKey' :  key}
+                }
+            })
+            return products
+        } else {
+            const products = await $fetch(url)
+            return products
+        }        
+    } catch (err) {
+        console.log(`err[${err}]`)
+        return err
+    }
+}
---
+++ pages/Nuxt3Cors/nvd_api.vue
@@ -0,0 +1,90 @@
+<template>
+    <div>
+        <v-card
+        class="mx-auto"
+        >
+            <v-card-text>
+                <p class="text-h6 font-weight-black">Request NVD API Key</p>
+        
+                <div class="text-medium-emphasis">
+                    All requests to the APIs use the HTTP GET method and each API has a unique base URL. 
+                    Parameters may be added to the URL query to filter requests for specific information. 
+                    The parameters function similar to those found on the NVD’s advanced CVE search page and the CVE/CPE details pages. 
+                    All parameter names and values are case insensitive.
+                    API keys are passed in the request header. 
+                    The exact method of passing header information with a GET request varies based on the user agent. 
+                    Including apiKey:{key value}, (without brackets or spaces) allows users to make a greater number of requests in a given time than they could otherwise. Please note, the {key value} is case sensitive.
+                </div>
+            </v-card-text>
+    
+            <v-card-actions>
+                <v-btn
+                    color="teal-accent-4"
+                    text="Request"
+                    variant="text"
+                    href="https://nvd.nist.gov/developers/request-an-api-key"
+                ></v-btn>
+            </v-card-actions>
+        </v-card>
+        <v-card
+            class="mx-auto"
+        >
+            <v-card-text>
+                <p class="text-h6 font-weight-black">Register NVD API Key</p>        
+                <v-text-field
+                    v-model="key"
+                    hide-details="auto"
+                    label="API Key"
+                    clearable
+                ></v-text-field>
+            </v-card-text>
+        </v-card>
+        <v-card
+            class="mx-auto"
+        >
+            <v-card-text>
+                <p class="text-h6 font-weight-black">Get CPE</p>        
+                <v-text-field
+                    v-model="cpeMatchString"
+                    hide-details="auto"
+                    label="cpeMatchString"
+                    clearable
+                ></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+                <v-btn
+                    color="teal-accent-4"
+                    :loading="processing"
+                    @click="getCpe"
+                >GET</v-btn>
+            </v-card-actions>
+            <v-card-text>
+                <v-textarea
+                    v-model="products"
+                    hide-details="auto"
+                    label="Products:"
+                    clearable
+                    bg-color="amber-lighten-4"
+                ></v-textarea>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+<script setup>
+    import { ref } from 'vue'
+    import { useCpeMatching } from '@/composables/useCpeMatching'
+    const processing = ref(false)
+    const key = ref('36d60302-e1bb-453b-b679-6828010ec8d9')
+    const cpeMatchString = ref('https://services.nvd.nist.gov/rest/json/cpes/2.0?cpeMatchString=cpe:2.3:o:microsoft:windows_10')
+    const products = ref('')
+    const withApiKey = ref('false')
+
+    const getCpe = async () => {
+        processing.value = true
+        const data = await useCpeMatching(cpeMatchString.value, key.value)
+        if (data) {
+            products.value = JSON.stringify(data)
+        }
+        processing.value = false
+    }
+</script>
